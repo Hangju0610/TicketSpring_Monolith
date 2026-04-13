581556c9a8ade6f4d423106cc544eb4b11835d37
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2026-04-13 16:09:06 +0900

    init: TicketSpring-Monolith project

---
+++ src/main/java/com/TicketSpring_Monolith/TicketSpringMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.TicketSpring_Monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSpringMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketSpringMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TicketSpring_Monolith/TicketSpringMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TicketSpring_Monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSpringMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
